2697e5ceada7ed7132502b16189856b1dc165978
John P Tomanelli II <jptomanelli@gmail.com>
2017-02-07 10:19:29 -0500

    Init backend socket.io server

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response){
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('hello world');
+  response.end();
+});
+
+server.listen(8080, () => {
+  console.log("server Running");
+});
