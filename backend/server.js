d4a3cdf16bd6b92144637c8e792932c7b3e719cc
John P Tomanelli II <jptomanelli@gmail.com>
2017-02-07 16:23:43 -0500

    Start communication
    * Start user list
    * Start messages

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use("/public", express.static(__dirname + '/public'));
 
@@ -9,18 +9,23 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html');
 });
 
-//Whenever someone connects this gets executed
 io.on('connection', function(socket){
+
   console.log('A user ' + socket.id + ' connected');
 
-  socket.emit('announcement', { message: 'A new user has joined!' });
-
-  socket.on('event', function(data) {
-    console.log('A client name is:', data.message);
+  socket.on('join', function(user) {
+    console.log(socket.id + ' set his username to ' + user);
+    io.sockets.emit('user.add', user);
   });
 
+  socket.on('chat.message', function(message){
+        console.log(socket.id + ' : ' + message);
+        io.sockets.emit('chat.message', message);
+    });
+
   socket.on('disconnect', function () {
-    console.log('\x1b[36m', 'A user ' + socket.id + ' disconnected', '\x1b[0m');
+    console.log('\x1b[36m', socket.id + ' disconnected', '\x1b[0m');
+    io.sockets.emit('user.remove', socket.id);
   });
 
 });
