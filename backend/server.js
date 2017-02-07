87e4afe7d3fea84b52eddfd6b282a7eb698af4ed
John P Tomanelli II <jptomanelli@gmail.com>
2017-02-07 15:02:23 -0500

    Create express server
    * Creating webpage for socket.io communication before adding to
    electron application

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,31 @@
-var http = require('http');
+var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var server = http.createServer(function(request, response){
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write('hello world');
-  response.end();
+app.use("/public", express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-server.listen(8080, () => {
-  console.log("server Running");
+//Whenever someone connects this gets executed
+io.on('connection', function(socket){
+  console.log('A user ' + socket.id + ' connected');
+
+  socket.emit('announcement', { message: 'A new user has joined!' });
+
+  socket.on('event', function(data) {
+    console.log('A client name is:', data.message);
+  });
+
+  socket.on('disconnect', function () {
+    console.log('\x1b[36m', 'A user ' + socket.id + ' disconnected', '\x1b[0m');
+  });
+
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
